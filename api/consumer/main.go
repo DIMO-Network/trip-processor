7f5df8b2ccc772d1b0bd534e84cf4bc041780d61
Dylan Moreland <dylan@dimo.zone>
2022-09-06 13:52:28 -0400

    Make a mess

--- api/consumer/main.go
+++ api/consumer/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,16 +10,15 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/DIMO-Network/shared"
 	_ "github.com/lib/pq"
 	"github.com/lovoo/goka"
-	"github.com/lovoo/goka/codec"
 )
 
 var (
-	brokers                = []string{"localhost:9092"}
-	topic      goka.Stream = "topic.device.status"
-	newRecords goka.Stream = "new_record"
-	group      goka.Group  = "mini-group"
+	brokers             = []string{"localhost:9092"}
+	topic   goka.Stream = "topic.device.status"
+	group   goka.Group  = "mini-group"
 
 	tmc *goka.TopicManagerConfig
 
@@ -54,8 +52,6 @@ type tripPointinTime struct {
 	Timestamp time.Time
 	Speed     float64
 }
-
-type DataCodec struct{}
 
 type DeviceTrip struct {
 	Id         string
@@ -100,105 +96,82 @@ func (pgc *PostgresController) StoreTrip(trp DeviceTrip) error {
 	return nil
 }
 
-// Encodes a trip into []byte
-func (jc *DataCodec) Encode(value interface{}) ([]byte, error) {
-	if _, istrip := value.(*Data); !istrip {
-		return nil, fmt.Errorf("Codec requires value *data, got %T", value)
-	}
-	return json.Marshal(value)
-}
-
-// Decodes a trip from []byte to it's go representation.
-func (jc *DataCodec) Decode(data []byte) (interface{}, error) {
-	var decoded Data
-	err := json.Unmarshal(data, &decoded)
-	if err != nil {
-		return nil, fmt.Errorf("Error unmarshaling data: %v", err)
-	}
-	return &decoded, nil
-}
-
-type newRecordCodec struct{}
-
-func (c *newRecordCodec) Encode(value interface{}) ([]byte, error) {
-	if _, istrip := value.(*DeviceTrip); !istrip {
-		return nil, fmt.Errorf("new record codec requires value *data, got %T", value)
-	}
-	return json.Marshal(value)
-}
-
-func (c *newRecordCodec) Decode(data []byte) (interface{}, error) {
-	var decoded DeviceTrip
-	err := json.Unmarshal(data, &decoded)
-	if err != nil {
-		return nil, fmt.Errorf("Error unmarshaling new record data: %v", err)
-	}
-	return &decoded, nil
-}
+var deviceStatusCodec = &shared.JSONCodec[Data]{}
+var tripStateCodec = &shared.JSONCodec[DeviceTrip]{}
 
 // Emit messages forever every second
-func runEmitter() {
-	emitter, err := goka.NewEmitter(brokers, topic, new(codec.String))
-	if err != nil {
-		log.Fatalf("error creating emitter: %v", err)
-	}
-	defer emitter.Finish()
-	for {
-		time.Sleep(1 * time.Second)
-		err = emitter.EmitSync("some-key", "some-value")
-		if err != nil {
-			log.Fatalf("error emitting message: %v", err)
+// func runEmitter() {
+// 	emitter, err := goka.NewEmitter(brokers, topic, new(codec.String))
+// 	if err != nil {
+// 		log.Fatalf("error creating emitter: %v", err)
+// 	}
+// 	defer emitter.Finish()
+// 	for {
+// 		time.Sleep(1 * time.Second)
+// 		err = emitter.EmitSync("some-key", "some-value")
+// 		if err != nil {
+// 			log.Fatalf("error emitting message: %v", err)
+// 		}
+// 	}
+// }
+
+const TripGracePeriod = 15 * time.Minute
+
+func processDeviceStatus(ctx goka.Context, msg any) {
+	var existingTrip *DeviceTrip
+	newDeviceStatus := msg.(*Data)
+
+	if val := ctx.Value(); val != nil {
+		existingTrip = val.(*DeviceTrip)
+
+		if newDeviceStatus.Data.Timestamp.Sub(existingTrip.LastActive) <= TripGracePeriod {
+			if newDeviceStatus.Data.Speed > 0 {
+				existingTrip.LastActive = newDeviceStatus.Data.Timestamp
+				ctx.SetValue(existingTrip)
+			}
+		} else {
+			// Grace period for the existing trip has passed, so need to end it before
+			// doing anything else.
+			// Trying to squash trips consisting of one data point.
+			if existingTrip.Start != existingTrip.LastActive {
+				existingTrip.End = existingTrip.LastActive
+				// TODO: Emit trip end event.
+				err := pgController.StoreTrip(*existingTrip)
+				if err != nil {
+					log.Fatal(err)
+				}
+				ctx.Delete()
+			}
+			if newDeviceStatus.Data.Speed > 0 {
+				ts := newDeviceStatus.Data.Timestamp.UTC()
+				beginTrip := &DeviceTrip{
+					Id:         ctx.Key(),
+					Start:      ts,
+					LastActive: ts,
+				}
+				ctx.SetValue(beginTrip)
+				// TODO: Emit trip start event.
+			}
 		}
+
+	} else {
+		ts := newDeviceStatus.Data.Timestamp.UTC()
+		beginTrip := &DeviceTrip{
+			Id:         ctx.Key(),
+			Start:      ts,
+			LastActive: ts,
+		}
+		ctx.SetValue(beginTrip)
 	}
 }
 
 // process messages until ctrl-c is pressed
 func runProcessor() {
-	cb := func(ctx goka.Context, msg interface{}) {
-
-		var existingRecord *DeviceTrip
-		newRecord := msg.(*Data)
-
-		if val := ctx.Value(); val != nil {
-			existingRecord = val.(*DeviceTrip)
-
-			if newRecord.Data.Timestamp.Sub(existingRecord.LastActive).Minutes() <= 15 {
-				if newRecord.Data.Speed > 0 {
-					existingRecord.LastActive = newRecord.Data.Timestamp
-					ctx.SetValue(existingRecord)
-				}
-			} else {
-				if existingRecord.Start != existingRecord.LastActive {
-					existingRecord.End = existingRecord.LastActive
-					err := pgController.StoreTrip(*existingRecord)
-					if err != nil {
-						log.Fatal(err)
-					}
-					ctx.Delete()
-				}
-				if newRecord.Data.Speed > 0 {
-					beginTrip := new(DeviceTrip)
-					beginTrip.Id = ctx.Key()
-					beginTrip.Start = newRecord.Data.Timestamp.UTC()
-					beginTrip.LastActive = beginTrip.Start
-					ctx.SetValue(beginTrip)
-				}
-			}
-
-		} else {
-			beginTrip := new(DeviceTrip)
-			beginTrip.Id = ctx.Key()
-			beginTrip.Start = newRecord.Data.Timestamp.UTC()
-			beginTrip.LastActive = beginTrip.Start
-			ctx.SetValue(beginTrip)
-		}
-	}
-
 	// Define a new processor group. The group defines all inputs, outputs, and
 	// serialization formats. The group-table topic is "example-group-table".
 	g := goka.DefineGroup(group,
-		goka.Input(topic, new(DataCodec), cb),
-		goka.Persist(new(newRecordCodec)),
+		goka.Input(topic, deviceStatusCodec, processDeviceStatus),
+		goka.Persist(tripStateCodec),
 	)
 
 	p, err := goka.NewProcessor(brokers, g,
--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module trips-api
 go 1.18
 
 require (
-	github.com/Shopify/sarama v1.34.1
+	github.com/DIMO-Network/shared v0.4.0
 	github.com/gofiber/fiber/v2 v2.36.0
-	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.6
 	github.com/lovoo/goka v1.1.7
-	github.com/tidwall/gjson v1.14.1
 )
 
 require (
+	github.com/Shopify/sarama v1.34.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/avast/retry-go/v4 v4.0.3 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.16.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/kms v1.17.3 // indirect
+	github.com/aws/smithy-go v1.11.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
@@ -30,15 +35,15 @@ require (
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/klauspost/compress v1.15.6 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
-	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
